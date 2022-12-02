d57063a5fbfabe3e119a37782297c2b56274688d
David james Reed <dreed@drift.com>
2022-12-01 19:02:42 -0500

    Merge into one file, I give up on go package errors

--- day1/main.go
+++ day1/main.go
@@ -51,5 +51,74 @@ func main() {
 	requestedCount := 3
 
 	fmt.Printf("Top %d highest calorie counts: %v\n", requestedCount, sortedCalorieSumList[len(sortedCalorieSumList)-requestedCount:])
-	fmt.Printf("Top %d Elves are carrying a total of %d calories\n", requestedCount, Sum(sortedCalorieSumList[len(sortedCalorieSumList)-requestedCount:]))
+
+	partialSum := Sum(sortedCalorieSumList[len(sortedCalorieSumList)-requestedCount:])
+	fmt.Printf("Top %d Elves are carrying a total of %d calories\n", requestedCount, partialSum)
+}
+
+func Max(list []int) int {
+	switch len(list) {
+	case 0:
+		return 0
+	default:
+		max := list[0]
+		for _, val := range list[0:] {
+			if max < val {
+				max = val
+			}
+		}
+		return max
+	}
+}
+
+func Sum(list []int) int {
+	result := 0
+	for _, v := range list {
+		result += v
+	}
+	return result
+}
+
+func MergeSort(list []int) []int {
+	listLen := len(list)
+	switch listLen {
+	case 0:
+		return nil
+	case 1:
+		return list
+	default:
+		halfLen := listLen / 2
+		leftSide := list[0:halfLen]
+		rightSide := list[halfLen:]
+
+		mergedLeft := MergeSort(leftSide)
+		mergedRight := MergeSort(rightSide)
+
+		return merge(mergedLeft, mergedRight)
+	}
+}
+
+// REQUIREMENT: la and lb are internally-sorted in ascending order
+func merge(la, lb []int) []int {
+	var mergedList []int
+
+	for ia, ib := 0, 0; ia <= len(la) || ia <= len(lb); {
+		if ia == len(la) {
+			mergedList = append(mergedList, lb[ib:]...)
+			break
+		} else if ib == len(lb) {
+			mergedList = append(mergedList, la[ia:]...)
+			break
+		} else {
+			if la[ia] > lb[ib] {
+				mergedList = append(mergedList, lb[ib])
+				ib++
+			} else {
+				mergedList = append(mergedList, la[ia])
+				ia++
+			}
+		}
+	}
+
+	return mergedList // Dead line, needed for compilation
 }
