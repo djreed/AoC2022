4d8fbc5e454987b6b6429840c759b86f3c6a2d30
David James Reed <ddavidreed@gmail.com>
2022-12-13 15:26:00 -0500

    day12

---
+++ day12/README.md
@@ -0,0 +1,5 @@
+For Part 1, you want to BFS from the starting node in the grid looking for the end.
+
+In Part 2, all that needs to change is that you search from the end to any of the valid starts.
+
+BFS in both cases ensures the first result you find is [one of] the shortest path(s).
---
+++ day12/part1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+grid = [list(x) for x in open(0).read().strip().splitlines()]
+
+for r, row in enumerate(grid):
+    for c, item in enumerate(row):
+        if item == "S":
+            startR = r
+            startC = c
+            grid[r][c] = "a"
+        if item == "E":
+            endR = r
+            endC = c
+            grid[r][c] = "z"
+
+queue = deque()
+queue.append((0, startR, startC))
+
+visited = {(startR, startC)}
+
+while queue:
+    dist, row, col = queue.popleft()
+    for newR, newC in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+        if newR < 0 or newC < 0 or newR >= len(grid) or newC >= len(grid[0]):
+            continue
+        if (newR, newC) in visited: # If we've already visited the target cell on this path
+            continue
+        if ord(grid[newR][newC]) - ord(grid[row][col]) > 1: # If the height of the next is more than one higher
+            continue
+        if newR == endR and newC == endC: # Found the end
+            print(dist + 1)
+            exit(0)
+        visited.add((newR, newC))
+        queue.append((dist + 1, newR, newC))
